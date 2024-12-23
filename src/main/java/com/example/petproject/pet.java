9383d582da013d2194e5b75ae521d57ef7d531b3
ziyadhussain23 <ziyadhussain23@gmail.com>
2024-12-23 20:14:01 +0530

    pet project

--- src/main/java/com/example/petproject/pet.java
+++ src/main/java/com/example/petproject/pet.java
@@ -2,10 +2,8 @@ package com.example.petproject;
 
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
+
 import jakarta.persistence.Id;
-import org.hibernate.annotations.processing.SQL;
 import org.springframework.format.annotation.DateTimeFormat;
 
 
